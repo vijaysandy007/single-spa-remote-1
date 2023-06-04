0b4aaa0ed40ace68f73a7ce3643bc93063df2cf1
vijay p <vijaysathya454@gmail.com>
2023-06-04 23:00:48 +0530

    intial commit

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode,NgZone } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { singleSpaAngular } from 'single-spa-angular';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+
+const lifecycles = singleSpaAngular({
+  bootstrapFunction: () => platformBrowserDynamic().bootstrapModule(AppModule),
+  template: '<app-root></app-root>',
+  NgZone
+});
+
+export const bootstrap = lifecycles.bootstrap;
+export const mount = lifecycles.mount;
+export const unmount = lifecycles.unmount;
 
 if (environment.production) {
   enableProdMode();
 }
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
